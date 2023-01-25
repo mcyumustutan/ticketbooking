05c80744617a0ca942643cf5f9c5d8b426b5ab50
Mehmet Can <mehmetcy01@gmail.com>
2023-01-25 17:00:59 +0300

    User ve rolleri repository interface oluşturuldu

---
+++ src/main/java/com/mcan/ticketbooking/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.mcan.ticketbooking.repository;
+
+import com.mcan.ticketbooking.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String username);
+}
