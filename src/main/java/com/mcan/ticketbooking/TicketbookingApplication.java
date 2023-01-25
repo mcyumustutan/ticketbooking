2c335a81c7744a469f5b010e5f662fc213a6b417
Mehmet Can <mehmetcy01@gmail.com>
2023-01-25 16:48:15 +0300

    Proje oluşturuldu.

---
+++ src/main/java/com/mcan/ticketbooking/TicketbookingApplication.java
@@ -0,0 +1,13 @@
+package com.mcan.ticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcan/ticketbooking/TicketbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcan.ticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
