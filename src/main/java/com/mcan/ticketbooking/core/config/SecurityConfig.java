a5968f223f2df6e392c57deaea0a86a102a7ac2b
Mehmet Can <mehmetcy01@gmail.com>
2023-01-25 17:07:34 +0300

    security ile ilgili ayarlar eklendi.

---
+++ src/main/java/com/mcan/ticketbooking/core/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.mcan.ticketbooking.core.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Autowired
+    AuthenticationSuccessHandler successHandler;
+
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+
+                .antMatchers("/client").hasRole("USER")
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/simple").permitAll()
+                .antMatchers("/home").permitAll()
+
+                .anyRequest()
+                .authenticated()
+
+                .and()
+                .headers().frameOptions().sameOrigin()
+
+                .and()
+                .formLogin()
+                .successHandler(successHandler)
+                .and()
+
+                .build();
+    }
+}
