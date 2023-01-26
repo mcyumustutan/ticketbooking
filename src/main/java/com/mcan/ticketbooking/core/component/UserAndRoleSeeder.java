b8e039bc005f5e37db7e8b49187086f40039369e
Mehmet Can <mehmetcy01@gmail.com>
2023-01-26 20:59:08 +0300

    Demo bilgiler için düzenleme

--- src/main/java/com/mcan/ticketbooking/core/component/UserAndRoleSeeder.java
+++ src/main/java/com/mcan/ticketbooking/core/component/UserAndRoleSeeder.java
@@ -1,16 +1,20 @@
 package com.mcan.ticketbooking.core.component;
 
-import com.mcan.ticketbooking.model.Role;
-import com.mcan.ticketbooking.model.User;
+import com.mcan.ticketbooking.model.*;
 import com.mcan.ticketbooking.repository.RoleRepository;
+import com.mcan.ticketbooking.repository.TicketRepository;
 import com.mcan.ticketbooking.repository.UserRepository;
+import com.mcan.ticketbooking.repository.UserTicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -24,6 +28,14 @@ public class UserAndRoleSeeder implements ApplicationRunner {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @Autowired
+    private UserTicketRepository userTicketRepository;
+
+    private LocalDate globalStartDate;
+    private LocalDate globalFinishDate;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -43,8 +55,8 @@ public class UserAndRoleSeeder implements ApplicationRunner {
 
         User admin = User.builder()
                 .id(1L)
-                .name("mcanadmin")
-                .email("mcan@g.com")
+                .name("admin")
+                .email("admin@g.com")
                 .password(passwordEncoder.encode("12345678"))
                 .roles(adminRoleList).build();
         userRepository.save(admin);
@@ -55,6 +67,93 @@ public class UserAndRoleSeeder implements ApplicationRunner {
                 .password(passwordEncoder.encode("12345678"))
                 .roles(userRoleList).build();
         userRepository.save(user);
+
+        User user1 = User.builder().id(2L)
+                .name("democan")
+                .email("demo@g.com")
+                .password(passwordEncoder.encode("12345678"))
+                .roles(userRoleList).build();
+        userRepository.save(user1);
+
+
+        this.globalStartDate = LocalDate.now().minusDays(50);
+        this.globalFinishDate = LocalDate.now().plusDays(50);
+
+        Ticket ticket1 = Ticket.builder()
+                .id(1L)
+                .start("Adana")
+                .finish("İstabul")
+                .seatQty(65)
+                .price(BigDecimal.valueOf(465))
+                .availableStart(this.globalStartDate)
+                .availableFinish(this.globalFinishDate)
+                .build();
+        ticketRepository.save(ticket1);
+
+        Ticket ticket2 = Ticket.builder()
+                .id(1L)
+                .start("Adana")
+                .finish("Ankara")
+                .seatQty(60)
+                .price(BigDecimal.valueOf(405))
+                .availableStart(this.globalStartDate)
+                .availableFinish(this.globalFinishDate)
+                .build();
+        ticketRepository.save(ticket2);
+
+        Ticket ticket3 = Ticket.builder()
+                .id(1L)
+                .start("Adana")
+                .finish("Eskişehir")
+                .seatQty(40)
+                .price(BigDecimal.valueOf(355))
+                .availableStart(this.globalStartDate)
+                .availableFinish(this.globalFinishDate)
+                .build();
+        ticketRepository.save(ticket3);
+
+        Ticket ticket4 = Ticket.builder()
+                .id(1L)
+                .start("Adana")
+                .finish("İzmir")
+                .seatQty(40)
+                .price(BigDecimal.valueOf(455))
+                .availableStart(this.globalStartDate)
+                .availableFinish(LocalDate.now().plusDays(50))
+                .build();
+        ticketRepository.save(ticket4);
+
+
+        List<User> demoUsers = userRepository.findAll();
+        List<Ticket> demoTickets = ticketRepository.findAll();
+
+        UserTicket UserTicket1 = UserTicket.builder()
+                .user(demoUsers.get(1))
+                .ticket(demoTickets.get(1))
+                .reservedAt(LocalDate.now())
+                .reservedQty(1)
+                .status(TicketStatus.RESERVED)
+                .build();
+        userTicketRepository.save(UserTicket1);
+
+        UserTicket UserTicket2 = UserTicket.builder()
+                .user(demoUsers.get(1))
+                .ticket(demoTickets.get(2))
+                .reservedAt(LocalDate.now())
+                .reservedQty(1)
+                .status(TicketStatus.POSTPONED)
+                .build();
+        userTicketRepository.save(UserTicket2);
+
+        UserTicket UserTicket3 = UserTicket.builder()
+                .user(demoUsers.get(2))
+                .ticket(demoTickets.get(2))
+                .reservedAt(LocalDate.now())
+                .reservedQty(1)
+                .status(TicketStatus.RESERVED)
+                .build();
+        userTicketRepository.save(UserTicket3);
+
 
 
     }
