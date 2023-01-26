aefbffd001ae86b76e558d95c34b4369eb917d70
Mehmet Can <mehmetcy01@gmail.com>
2023-01-26 11:45:26 +0300

    Ticket repository oluşturuldu

---
+++ src/main/java/com/mcan/ticketbooking/repository/TicketRepository.java
@@ -0,0 +1,10 @@
+package com.mcan.ticketbooking.repository;
+
+import com.mcan.ticketbooking.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+}
